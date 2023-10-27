09452ac1e6f5529c43c624271f87ecb4c96b0f93
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2023-10-27 16:05:12 +0300

    first attempt

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,52 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  private tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Wellcome!");
+    } else {
+      console.log("The door is locked.");
+    }
+  }
+}
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Wellcome!");
+    } else {
+      console.log("The door is locked! Try another key!");
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -9,3 +58,14 @@ house.comeIn(person);
 
 
 export {};
+
+
+
+
+
+
+
+
+
+
+
